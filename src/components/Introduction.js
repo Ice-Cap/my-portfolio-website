aa472944ff3ff2f227a1bc063eedfd9245f708e2
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2022-02-05 12:50:02 -0700

    Layout for the introduction section

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,9 +1,35 @@
+import jsLogo from '../images/javascript-logo.png';
+import reactLogo from '../images/react-logo.png';
+import gitLogo from '../images/git-logo.png';
+import sassLogo from '../images/sass-logo.png';
+import bootstrapLogo from '../images/bootstrap-logo.png';
+import phpLogo from '../images/php-logo.png';
+import postgre from '../images/postgre.png';
 
 function Introduction() {
   return (
     <section id="introduction">
-      <h1>Hi, my name is Canyon</h1>
-      <p>I'm a web developer with a strong passion for building things</p>
+      <div className="top-container">
+        <div className="heading-container">
+          <h1>Hi, my name is Canyon</h1>
+          <p>I'm a web developer with a strong passion for building things</p>
+        </div>
+        <div className="cube">
+
+        </div>
+      </div>
+      <h3 className="tools-heading">These are my favorite tools</h3>
+      <div className="tools">
+        <div className='text-center'>
+          <img src={jsLogo} alt="JavaScript Logo"/>
+          <img src={reactLogo} alt="React Logo"/>
+          <img src={gitLogo} alt="Git Logo"/>
+          <img src={sassLogo} alt="Sass Logo"/>
+          <img src={bootstrapLogo} alt="Bootstrap Logo"/>
+          <img src={phpLogo} alt="PHP Logo"/>
+          <img src={postgre} alt="PostgreSQL Logo"/>
+        </div>
+      </div>
     </section>
   )
 }
