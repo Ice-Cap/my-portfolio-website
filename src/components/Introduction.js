7984ec097885b9bc1d9df1ecda50bb4b842d8623
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2022-02-05 11:35:57 -0700

    initial commit

---
+++ src/components/Introduction.js
@@ -0,0 +1,11 @@
+
+function Introduction() {
+  return (
+    <section id="introduction">
+      <h1>Hi, my name is Canyon</h1>
+      <p>I'm a web developer with a strong passion for building things</p>
+    </section>
+  )
+}
+
+export default Introduction;
