b8beadc26d19ae03f3604dbe0c30c74217eb249c
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2022-02-08 17:52:55 -0700

    Started on contact section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Introduction from "./components/Introduction";
 import Projects from './components/Projects';
 import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Introduction />
       <Projects />
       <About />
+      <Contact />
     </main>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <div className="container">
+        <div className="contact-container">
+          <div className="section-heading">
+            <h2>Contact</h2>
+          </div>
+          <form action="">
+            <input type="text" />
+            <input type="text" />
+            <textarea name="" id="" ></textarea>
+            <button>Send</button>
+          </form>
+        </div>
+        <div className="socials-container">
+          <div className="section-heading">
+            <h2>Socials</h2>
+          </div>
+          <div className="social-logos">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Contact;
