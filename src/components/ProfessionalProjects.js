11bb6fa08fc0a82973be81cda807cbdc6edf8e7e
Ice-Cap <66086363+Ice-Cap@users.noreply.github.com>
2022-02-11 12:56:48 -0700

    Added info and pictures to professional projects section and updated projects styling

--- src/components/ProfessionalProjects.js
+++ src/components/ProfessionalProjects.js
@@ -10,28 +10,28 @@ function ProfessionalProjects() {
       <div className="background-design"></div>
       <div className="projects-container">
         <ProjectCard
-          title={'The Lotus and The Lion'}
-          shortDescription={'Website for a life coach'}
-          previewLink={'https://thelotusandthelion.net/'}
-          codeLink={'https://github.com/Ice-Cap/level-up'}
-          technologiesUsed={'Built with HTML, CSS, and JavaScript'}
-          fullDescription={'The purpose of this app is to help people see progress within their skills.'}
-        />
-        <ProjectCard
           title={'Investing Lynx'}
           shortDescription={'Investing information website'}
-          previewLink={'https://poketeambuilder.io/'}
+          previewLink={'https://investinglynx.com/'}
           codeLink={'https://github.com/Ice-Cap/poke-team-builder'}
-          technologiesUsed={'Built with HTML, CSS, and JavaScript'}
-          fullDescription={'This is a pokémon team builder that helps people build better teams for competetive pokémon.'}
+          technologiesUsed={'Built with HTML, CSS, Bootstrap, and JavaScript.'}
+          fullDescription={"InvestingLynx.com is for people wanting to enter the investing space, but aren't sure where to start."}
         />
         <ProjectCard
           title={'Brave Submarines'}
           shortDescription={'Website for a local band'}
-          previewLink={'https://superpaint.netlify.app/'}
+          previewLink={'https://bravesubmarines.com/'}
           codeLink={'https://github.com/Ice-Cap/super-paint'}
-          technologiesUsed={'Built with HTML, CSS, and JavaScript.'}
-          fullDescription={'This is a simple browser based drawing app created with vanilla JavaScript and the canvas API.'}
+          technologiesUsed={'Built with HTML, SCSS, and JavaScript.'}
+          fullDescription={'BraveSubmarines.com showcases a local bands music and provides information about the band.'}
+        />
+        <ProjectCard
+          title={'The Lotus and The Lion'}
+          shortDescription={'Website for a life coach'}
+          previewLink={'https://thelotusandthelion.net/'}
+          codeLink={'https://github.com/Ice-Cap/level-up'}
+          technologiesUsed={'Built with HTML, SCSS, JavaScript, and PHP.'}
+          fullDescription={'TheLotusAndTheLion.net provides information on their life coaching services and links to schedule life coaching sessions.'}
         />
       </div>
     </section>
